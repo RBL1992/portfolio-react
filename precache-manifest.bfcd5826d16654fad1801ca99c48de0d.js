94a53d852a3973967100f8c9061bf68a0a4244ff
Roy <loganroyjr4@gmail.com>
2022-11-02 14:57:58 -0500

    Updates

---
+++ precache-manifest.bfcd5826d16654fad1801ca99c48de0d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "783611d3b3e63c99748b",
+    "url": "/my-app/static/css/main.71067cd0.chunk.css"
+  },
+  {
+    "revision": "783611d3b3e63c99748b",
+    "url": "/my-app/static/js/main.783611d3.chunk.js"
+  },
+  {
+    "revision": "ca198d2aedda01a51c5a",
+    "url": "/my-app/static/js/1.ca198d2a.chunk.js"
+  },
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "e5c61c8843f079d1c523dc18d3098bd8",
+    "url": "/my-app/static/media/Roy B Logan Jr Resume (1).e5c61c88.pdf"
+  },
+  {
+    "revision": "b670e93f0ea6b8d356ff1cac2556e038",
+    "url": "/my-app/static/media/TeamSpeakLogo.b670e93f.png"
+  },
+  {
+    "revision": "d5d4777a47570455882d3025620ed516",
+    "url": "/my-app/static/media/jateLogo.d5d4777a.svg"
+  },
+  {
+    "revision": "e6ff5f94730479e5b9a358afad69f49c",
+    "url": "/my-app/static/media/readmeLogo.e6ff5f94.svg"
+  },
+  {
+    "revision": "94dcdb6fff03d78ead4af08c64f023f8",
+    "url": "/my-app/static/media/IMG_3023.94dcdb6f.JPG"
+  },
+  {
+    "revision": "f9e2b2a96a38d428c1d960b6c382f9df",
+    "url": "/my-app/index.html"
+  }
+];
