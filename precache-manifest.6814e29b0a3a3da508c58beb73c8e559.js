d9a938607e5af0d5bc3d3dff13f49559b6585502
Roy <loganroyjr4@gmail.com>
2022-11-02 19:28:10 -0500

    Updates

---
+++ precache-manifest.6814e29b0a3a3da508c58beb73c8e559.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a5407233fdc4822bffc",
+    "url": "/portfolio-react/static/css/main.e7b450fc.chunk.css"
+  },
+  {
+    "revision": "8a5407233fdc4822bffc",
+    "url": "/portfolio-react/static/js/main.8a540723.chunk.js"
+  },
+  {
+    "revision": "ca198d2aedda01a51c5a",
+    "url": "/portfolio-react/static/js/1.ca198d2a.chunk.js"
+  },
+  {
+    "revision": "9338e8a022e42c6bc02f",
+    "url": "/portfolio-react/static/js/runtime~main.9338e8a0.js"
+  },
+  {
+    "revision": "e5c61c8843f079d1c523dc18d3098bd8",
+    "url": "/portfolio-react/static/media/Roy B Logan Jr Resume (1).e5c61c88.pdf"
+  },
+  {
+    "revision": "b670e93f0ea6b8d356ff1cac2556e038",
+    "url": "/portfolio-react/static/media/TeamSpeakLogo.b670e93f.png"
+  },
+  {
+    "revision": "d5d4777a47570455882d3025620ed516",
+    "url": "/portfolio-react/static/media/jateLogo.d5d4777a.svg"
+  },
+  {
+    "revision": "e6ff5f94730479e5b9a358afad69f49c",
+    "url": "/portfolio-react/static/media/readmeLogo.e6ff5f94.svg"
+  },
+  {
+    "revision": "94dcdb6fff03d78ead4af08c64f023f8",
+    "url": "/portfolio-react/static/media/IMG_3023.94dcdb6f.JPG"
+  },
+  {
+    "revision": "53be9580da5c1fb9204358e8d507d25a",
+    "url": "/portfolio-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.bfcd5826d16654fad1801ca99c48de0d.js"
+  "/portfolio-react/precache-manifest.6814e29b0a3a3da508c58beb73c8e559.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/my-app/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio-react/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
