8beb12a25e8d7b5a6129c99a0ff0e9e864088b01
Roy <loganroyjr4@gmail.com>
2022-11-02 20:16:37 -0500

    revised anchors in project cards

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -11,16 +11,19 @@ function Projects() {
         <div>
             <ProjectCard
                 img={teamSpeakLogo}
+                name="TeamSpeak"
                 liveLink="https://teamspeak-org.herokuapp.com/"
                 gitHubLink="https://github.com/park-d/team-speak"
             />
             <ProjectCard
                 img={jatelogo}
+                name="J.A.T.E"
                 liveLink="https://frozen-ravine-97502.herokuapp.com/"
                 gitHubLink="https://github.com/RBL1992/text-editor-PWA"
             />
             <ProjectCard
                img={readmeLogo}
+               name=""
                liveLink="README Generator"
                gitHubLink="https://github.com/RBL1992/README-generator" 
             />
